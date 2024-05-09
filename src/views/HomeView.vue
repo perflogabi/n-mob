e1219e4faafee4c3b188d7343b964c66374960ff
Gabriela Florêncio <perflogabi@gmail.com>
2024-05-09 12:28:17 -0300

    feat: add logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="Home" class="flex flex-col h-screen">
     <div class="m-3 mb-auto">
+      <div class="my-20">
+        <img width="150" src="img/m-mob/logo.png">
+      </div>
 
       <div class="grid grid-cols-2 gap-3 my-3">
         <ServiceSelectLarge 
